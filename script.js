85662ba8b00f8120d21822dbe9aeab8d919ccba1
GianSen <giansenrockz@gmail.com>
2022-07-14 22:13:59 +0800

    Added buttons to finish reading book and to addd new book

--- script.js
+++ script.js
@@ -9,20 +9,31 @@ function Book(author,title,noofpages,readalr) {
 let cat=document.getElementById("catalogue")
 function addBookToLibrary(boo) {
   // do stuff here
-  const nboo=document.createElement("div")
-  nboo.classList.add("book")
+  const container=document.createElement("div")
+  container.classList.add("book")
   let texts=["Author :", "Title :","Number of pages :","Read before ? :"]
   let i=0
   for (const prop in boo){
-    nboo.innerHTML+=(texts[i]+" "+boo[prop])
-    nboo.innerHTML+="<br>"
-    nboo.innerHTML+="<br>"
+    container.innerHTML+=(texts[i]+" "+boo[prop])
+    container.innerHTML+="<br>"
+    container.innerHTML+="<br>"
     i+=1
-    
+
   }
-  cat.appendChild(nboo)
+  boo.index=myLibrary.indexOf(boo)
+  addTodisplay(boo,container)
 
 
+}
+function addTodisplay(book,container){
+    let cat=document.getElementById("catalogue")
+    const butt=document.createElement("button")
+    butt.textContent="Finished Reading ?"
+    butt.addEventListener("click",clearBookfromLibrary)
+    butt.value=book.index
+    container.setAttribute("id",book.index)
+    container.appendChild(butt)
+    cat.appendChild(container)
 }
 
 //test cases
@@ -35,8 +46,56 @@ const d=new Book("gs","deeznuts",100000,false)
 let myLibrary = [Mobydick,a,b,c,d];
 
 
-myLibrary.forEach(boo=>addBookToLibrary(boo))
+
+function createNewBook(){
+    let author=document.getElementById("author").value
+    let title=document.getElementById("titles").value
+    let pages=document.getElementById("pages").value
+    let readbefore=document.getElementById("readbefore").value
+    let newbook=new Book(author,title,pages,readbefore)
+    myLibrary.push(newbook)
+    addBookToLibrary(newbook)
+    document.getElementById("author").value=""
+    document.getElementById("titles").value=""
+    document.getElementById("pages").value=""
+    document.getElementById("readbefore").value=""
+}
+
+submitbutton=document.getElementById("submitbutton")
+submitbutton.addEventListener("click",createNewBook)
+
+document.getElementById("openform").addEventListener("click", openForm)
+function openForm() {
+    document.getElementById("addbook").style.display = "block";
+    document.getElementById("openform").style.display="none";
+  }
 
 
-button=document.getElementById('addbook')
-button.addEventListener("click",addBookToLibrary)
+document.getElementById("closeform").addEventListener("click", closeForm)
+function closeForm() {
+    document.getElementById("addbook").style.display = "none";
+    document.getElementById("openform").style.display="block";
+  }
+
+function clearBookfromLibrary(){
+    let index=this.value
+    temp=[]
+    let wantedbook=document.getElementById(index)
+    console.log(wantedbook)
+    wantedbook.remove()
+    if (myLibrary.length==1){
+        myLibrary=[]
+    }
+    else
+    {
+    for (let j=0;j<myLibrary.length;j++){
+        if (index!=j){
+            temp.push(myLibrary[j])
+
+        }
+    }
+    myLibrary=temp
+    console.log(myLibrary)
+}
+}
+  myLibrary.forEach(boo=>addBookToLibrary(boo))
