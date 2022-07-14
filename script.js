c66ba8b3929f243f598b10cad41a59ffc80fce49
GianSen <giansenrockz@gmail.com>
2022-07-14 17:26:23 +0800

    Added books object to catalogue id

--- script.js
+++ script.js
@@ -6,9 +6,23 @@ function Book(author,title,noofpages,readalr) {
     this.noofpages=noofpages;
     this.readalr=readalr;
 }
-
-function addBookToLibrary() {
+let cat=document.getElementById("catalogue")
+function addBookToLibrary(boo) {
   // do stuff here
+  const nboo=document.createElement("div")
+  nboo.classList.add("book")
+  let texts=["Author :", "Title :","Number of pages :","Read before ? :"]
+  let i=0
+  for (const prop in boo){
+    nboo.innerHTML+=(texts[i]+" "+boo[prop])
+    nboo.innerHTML+="<br>"
+    nboo.innerHTML+="<br>"
+    i+=1
+    
+  }
+  cat.appendChild(nboo)
+
+
 }
 
 //test cases
@@ -16,8 +30,13 @@ const Mobydick= new Book('Sherwin','MobyDick',60,false)
 const a= new Book('Sherwin','a',69,false)
 const b= new Book('Sherwin','b',60,false)
 const c= new Book('Sherwin','c',60,false)
+const d=new Book("gs","deeznuts",100000,false)
 
-let myLibrary = [Mobydick,a,b,c];
+let myLibrary = [Mobydick,a,b,c,d];
 
-let cat=document.getElementById("catalogue")
-myLibrary.forEach(boo=>cat.innerHTML+=Object.values(boo))
+
+myLibrary.forEach(boo=>addBookToLibrary(boo))
+
+
+button=document.getElementById('addbook')
+button.addEventListener("click",addBookToLibrary)
